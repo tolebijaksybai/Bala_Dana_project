68388d24b91c1966ddd08f881770e185d8c3608f
Tolebi <tolebizaksybaj@gmail.com>
2020-11-13 08:00:00 +0600

    project

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function () {
+	"use strict";
+
+	const register = document.querySelector('.register'),
+		overlay = document.querySelector('.overlay'),
+		popupClose = document.querySelector('.popup-close');
+
+	register.addEventListener('click', function () {
+		// this.style.display = "none";
+		overlay.style.display = "block";
+		document.body.style.overflow = "hidden";
+	});
+
+	popupClose.addEventListener('click', () => {
+		register.style.display = "block";
+		overlay.style.display = "none";
+		document.body.style.overflow = "";
+	});
+
+	const sendMessage = document.querySelector('.send-message'),
+		overlayy = document.querySelector('.overlayy'),
+		popupClosee = document.querySelector('.popup-closee');
+
+	sendMessage.addEventListener('click', function () {
+		// this.style.display = "none";
+		overlayy.style.display = "block";
+		document.body.style.overflow = "hidden";
+	});
+
+	popupClosee.addEventListener('click', () => {
+		sendMessage.style.display = "block";
+		overlayy.style.display = "none";
+		document.body.style.overflow = "";
+	});
+});
